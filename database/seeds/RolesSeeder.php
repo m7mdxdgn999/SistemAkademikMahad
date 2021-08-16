0aa4360484d962a13a033a54be62e9abffa0735a
m7mdxdgn999 <42533116+m7mdxdgn999@users.noreply.github.com>
2021-08-16 15:27:47 +0700

    memberikan role untuk user

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name'=>'admin'
+        ]);
+
+        Role::create([
+            'name'=>'mahasiswa'
+        ]);
+
+        Role::create([
+            'name'=>'dosen'
+        ]);
+    }
+}
