fce1ec38779ed14bdf3a519fb3d8727b8be40609
m7mdxdgn999 <42533116+m7mdxdgn999@users.noreply.github.com>
2021-08-16 19:09:26 +0700

    membuat user admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesSeeder::class);
+        $this->call([
+            RolesSeeder::class,
+            ::class
+
+        ]);
     }
 }
